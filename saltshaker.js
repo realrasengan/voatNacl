f86b40bbe5987e5064a08a545b87b6dd4e746741
realrasengan <andysl@gmail.com>
2019-02-19 19:31:31 -0800

    module saltshaker

--- saltshaker.js
+++ saltshaker.js
@@ -27,7 +27,7 @@
 //
 // THIS SOFTWARE IS UNAUDITED.  USE AT YOUR OWN RISK.
 
-var SaltShaker = (function() {
+(function(SaltShaker) {
 
   // Function:  _decoder.decode (internal)
   // Purpose:   converts an uint8array to a string
@@ -37,7 +37,7 @@ var SaltShaker = (function() {
   // Function:  create
   // Purpose:   creates a keypair (optionally from privatekey)
   // Returns:   {"publickey":publickey,"privatekey":privatekey}
-  var create = function(privatekey) {
+  SaltShaker.create = function(privatekey) {
     var _keys = null;
     
     if(privatekey)
@@ -53,14 +53,14 @@ var SaltShaker = (function() {
   // Function:  sign(msg, privkey)
   // Purpose:   uses a private key to sign a msg
   // Returns:   signed msg
-  var sign = function(msg, privkey) {
+  SaltShaker.sign = function(msg, privkey) {
     return nacl.util.encodeBase64(nacl.sign(nacl.util.decodeUTF8(msg),nacl.util.decodeBase64(privkey)));
   }
 
   // Function:  verify(signedmsg, pubkey)
   // Purpose:   uses a public key to verify a signed msg by the public key
   // Returns:   original msg derived from signed msg
-  var verify = function(signedmsg, pubkey) {
+  SaltShaker.verify = function(signedmsg, pubkey) {
     var _returnv = null;
 
     return ((_returnv = nacl.sign.open(nacl.util.decodeBase64(signedmsg),nacl.util.decodeBase64(pubkey))) ? _decoder.decode(_returnv) : null);
@@ -69,7 +69,7 @@ var SaltShaker = (function() {
   // Function:  encrypt(msg, pubkey, privkey)
   // Purpose:   uses a target's public key and a private key to encrypt a msg
   // Returns:   JSON object {"nonce":nonce,"message":msg}
-  var encrypt = function(msg, pubkey, privkey) {
+  SaltShaker.encrypt = function(msg, pubkey, privkey) {
     var _nonce = nacl.randomBytes(nacl.box.nonceLength);
 
     return {
@@ -81,15 +81,8 @@ var SaltShaker = (function() {
   // Function:  decrypt(msg, nonce, pubkey, privkey)
   // Purpose:   uses a target's pubkey and a private key to decrypt a msg
   // Returns:   original msg decrypted from the encrypted msg
-  var decrypt = function(msg, nonce, pubkey, privkey) {
+  SaltShaker.decrypt = function(msg, nonce, pubkey, privkey) {
     return _decoder.decode(nacl.box.open(nacl.util.decodeBase64(msg),nacl.util.decodeBase64(nonce),ed2curve.convertPublicKey(nacl.util.decodeBase64(pubkey)), ed2curve.convertSecretKey(nacl.util.decodeBase64(privkey))));
   }
 
-  return {
-    create: create,
-    sign: sign,
-    verify: verify,
-    encrypt: encrypt,
-    decrypt: decrypt
-  }
-})();
+})(typeof module !== 'undefined' && module.exports ? module.exports : (self.SaltShaker = self.SaltShaker || {}));
